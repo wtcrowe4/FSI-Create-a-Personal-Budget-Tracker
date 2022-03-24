7868514c9fca4e49302c7fbe4e35608bbb6a6c98
Thomas Crowe <wcrowe@g.clemson.edu>
2022-03-23 20:24:17 -0400

    add for loops

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+for(i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for(i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
